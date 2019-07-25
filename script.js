1036095e0a7b0820ef34f8de5c633c82caba84f8
Andy DuBois <andydubois89@gmail.com>
2019-07-25 09:34:44 -0500

    added parseFloat

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 console.log('js');
 
 function addinator (num1, num2) {
-    return num1 + num2;
+    return parseFloat(num1) + parseFloat(num2);
 }
 
 console.log('When I pass in 1 and 2 I expect 3. The result was: ', addinator(1, 2));
 
+console.log('When I pass in -1 and 2 I expect 1. The result was: ', addinator(-1, 2));
+
+console.log('When I pass in 1.5 and 1 I expect 2.5. The result was: ', addinator(1.5, 1));
+
+console.log('When I pass in "1" and "2" I expect 3. The result was: ', addinator('1', '2'));
